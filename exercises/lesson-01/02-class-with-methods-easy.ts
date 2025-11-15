7c0da35b5c39741dc4d82d761e4653f24a8f8ba7
LanceOS <lancehemphill2000@gmail.com>
2025-11-15 16:17:29 -0500

    lesson 2 completed

--- exercises/lesson-01/02-class-with-methods-easy.ts
+++ exercises/lesson-01/02-class-with-methods-easy.ts
@@ -9,3 +9,39 @@
  */
 
 // Your code here
+
+
+class Counter {
+    public count: number;
+    public original: number;
+
+    constructor(num: number = 0) {
+        this.count = num;
+        this.original = num;
+    }
+
+    public increment() {
+        return ++this.count;
+    }
+
+    public decrement() {
+        return --this.count;
+    }
+
+    public getValue() {
+        return this.count;
+    }
+
+    public reset() {
+        return this.count = this.original;
+    }
+}
+
+
+const counter = new Counter(5);
+
+console.log(counter);
+console.log("Incremented to: ", counter.increment());
+console.log("Decremented to: ", counter.decrement());
+console.log("Value is currently: ", counter.getValue());
+console.log("Original value is: ", counter.reset());
